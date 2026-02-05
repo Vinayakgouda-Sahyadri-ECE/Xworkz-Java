cde80adba1b55c98de951522bd9160ee879eb791
Vinayakgouda-Sahyadri-ECE <shadowvinayak@gmail.com>
2026-02-05 15:52:41 +0530

    shgs

---
+++ PassportSeva.java
@@ -0,0 +1,35 @@
+class PassportSeva{
+	
+static int dcdrLocation=5;
+static String email="Laluprasad25@gmail.com";
+static boolean emailLoginSame=true;
+static String givenName="Lalu Prasad B";
+static String password="judsef5ef545f5r454";
+static String registrationAutorithyMethod="Password";
+static String token="refeggggRG_RGRGRGER_GGRGERG_RGRGRgeffRG_RGRGRFRRREHYJTYJUUJ_JYThghty";
+static String userId="Laluprasad2504";	
+	
+
+public static void main(String[] args){
+
+
+
+System.out.println("show the location no:"+dcdrLocation);
+System.out.println("email:"+email);
+System.out.println("is login email same?"+emailLoginSame);
+System.out.println("given name for passport:"+givenName);
+System.out.println("password:"+password);
+System.out.println("registration authourity method is by:"+registrationAutorithyMethod);
+System.out.println("what is the token no:"+token);
+System.out.println("userid:"+userId);
+
+
+
+
+
+
+
+
+
+}
+}
---
+++ Trial.java
@@ -0,0 +1,15 @@
+class Trial{
+	
+	//static variable
+	static int dcdrLocation;
+	
+public static void main(String[] args){  //parameter variable                             
+//local variable
+
+dcdrLocation=5;
+
+System.out.println("show the location no:"+dcdrLocation);
+}
+}
+
+jn j n                                                                                                                            
